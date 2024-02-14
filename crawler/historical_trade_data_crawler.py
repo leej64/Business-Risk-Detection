fb8a24ffb871de741022fb21608ff0ead565ea14
tongr1 <rontong@protonmail.com>
2024-02-13 20:39:22 -0600

    Add latest scraping results

--- crawler/historical_trade_data_crawler.py
+++ crawler/historical_trade_data_crawler.py
@@ -8,8 +8,10 @@ import yfinance as yf
 from sp500_symbols import sp500_symbols 
 import time
 
-def write_to_local_csv(filename, df):
-    df.to_csv(filename, mode = "w", index = True)
+# Write dataframe to local csv
+def write_to_local_csv(filename, df, has_header):
+    df.to_csv(filename, sep = "`", mode = "a", index = True, header = has_header)
+
 
 if __name__ == "__main__":
     # List of companies to get info for
@@ -25,14 +27,20 @@ if __name__ == "__main__":
     # Start and end date of historical data
     start_date = "2018-01-01"
     end_date = "2018-12-31"
+    
+    need_header = True
 
     for symbol in sp500_symbols:
         tick = yf.Ticker(symbol)
         # Get historical data
         hist = tick.history(start = start_date, end = end_date)
-        #print(hist.columns)
+        hist["Symbol"] = symbol
+        print(hist.columns)
         # Write to local csv files
-        file_name = symbol + " - " + start_date + " - " + end_date + ".csv"
-        write_to_local_csv("./stockdata/" + file_name, hist)
+        file_name = "./stockdata/tradeData - " + start_date + " - " + end_date + ".csv"
+        # Write to local csv files
+        write_to_local_csv(file_name, hist, need_header)
         time.sleep(1)
+        if need_header == True:
+            need_header = False
 
